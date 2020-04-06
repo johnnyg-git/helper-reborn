6b9a81709a8b6b61b308e2f7b2d8eb6699ec1b12
johnnyg-git <55810567+johnnyg-git@users.noreply.github.com>
2020-04-06 22:11:18 +0100

    Add files via upload

---
+++ Helper Reborn Source/Helper Reborn/CommandData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Recognition;
+using BS;
+using Newtonsoft.Json;
+
+namespace Helper_Reborn
+{
+    [Serializable]
+    public class commandHandler
+    {
+        [JsonMergeKey]
+        [JsonProperty("$type")]
+        [JsonIgnore]
+        public Type type
+        {
+            get
+            {
+                return base.GetType();
+            }
+            set
+            {
+            }
+        }
+
+        public virtual void setupGrammar(SpeechRecognitionEngine recognitionEngine)
+        { }
+
+        public virtual void doCommand(RecognitionResult speechResult)
+        { }
+
+        public string[] commandPhrases;
+    }
+    [Serializable]
+    public class CommandData
+    {
+        public string id;
+        public commandHandler handler;
+    }
+
+    [Serializable]
+    public class CategoryData
+    {
+        [Serializable]
+        public class item
+        {
+            public string id;
+            public string[] recognisablePhrases;
+        }
+
+        public string id;
+        public List<item> members;
+    }
+}
